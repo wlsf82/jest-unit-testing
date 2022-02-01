7801ea2a0fdcce0008e7bbef8a68c8b824b7ab78
Walmyr <wlsf82@gmail.com>
2022-02-01 17:14:44 +0100

    Add new failing test
    Failure:
    Expected: 50
    Received: 0

--- index.spec.js
+++ index.spec.js
@@ -6,3 +6,14 @@ test('se a lista estiver vazia, a comissão é zero', () => {
 
   expect(resultadoAtual).toBe(resultadoEsperado)
 })
+
+it('calcula comissão quando só tem um item na lista', () => {
+  const resultadoAtual = calculaComissaoDeVenda([{
+    id: 'PROD-123',
+    precoUnitario: 1000,
+    quantidadeVendida: 1
+  }])
+  const resultadoEsperado = 50
+
+  expect(resultadoAtual).toBe(resultadoEsperado)
+})
