c47e1aee3f2a0bb679106832b5e29f7fe082ef29
Walmyr <wlsf82@gmail.com>
2022-02-01 17:27:27 +0100

    Make tests pass again

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 module.exports = function(items) {
   if (!items.length) return 0
-  const { precoUnitario, quantidadeVendida } = items[0]
-  return precoUnitario * quantidadeVendida * .05
+  if (items.length === 1) {
+    const { precoUnitario, quantidadeVendida } = items[0]
+    return precoUnitario * quantidadeVendida * .05
+  }
+  if (items.length > 1) {
+    const prices = items.map(item => {
+      const { precoUnitario, quantidadeVendida } = item
+      return precoUnitario * quantidadeVendida
+    })
+
+    const totalPrice = prices.reduce((a, b) => a + b)
+
+    return totalPrice * .05
+  }
+
 }
