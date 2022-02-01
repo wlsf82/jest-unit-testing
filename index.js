1140389b4f6fb0ecc9443981aa072ab90d67945c
Walmyr <wlsf82@gmail.com>
2022-02-01 17:54:08 +0100

    Add missing feature and make tests pass

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 module.exports = function(items) {
+  let foundExclusiveProduct = false
   const totalPrice = items.map(item => {
-    const { precoUnitario, quantidadeVendida } = item
+    const { id, precoUnitario, quantidadeVendida } = item
+    if (id === 'XP-0101') {
+      foundExclusiveProduct = true
+    }
     return precoUnitario * quantidadeVendida
   }).reduce((a, b) => a + b, 0)
 
+  if (foundExclusiveProduct) return totalPrice * .2
   if (totalPrice < 1999.99) return totalPrice * .05
   if (totalPrice > 1999.99 && totalPrice < 4999.99) return totalPrice * .1
   if (totalPrice > 4999.99) return totalPrice * .15
