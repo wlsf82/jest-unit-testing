4430bed567de931a254018dcaa16d307755e17f6
Walmyr <wlsf82@gmail.com>
2022-02-01 22:28:16 +0100

    Remove code duplications
    By creating a multifly function, and using it.

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ module.exports = function(items) {
   const totalPrice = items.map(item => {
     const { id, precoUnitario, quantidadeVendida } = item
     if (id === 'XP-0101') foundExclusiveProduct = true
-    return precoUnitario * quantidadeVendida
+    return multiply(precoUnitario, quantidadeVendida)
   }).reduce((a, b) => a + b, 0)
 
-  if (foundExclusiveProduct) return totalPrice * .2
-  if (totalPrice < 1999.99) return totalPrice * .05
-  if (totalPrice > 1999.99 && totalPrice < 4999.99) return totalPrice * .1
-  if (totalPrice > 4999.99) return totalPrice * .15
+  if (foundExclusiveProduct) return multiply(totalPrice, .2)
+  if (totalPrice < 1999.99) return multiply(totalPrice, .05)
+  if (totalPrice > 1999.99 && totalPrice < 4999.99) return multiply(totalPrice, .1)
+  if (totalPrice > 4999.99) return multiply(totalPrice, .15)
+}
+
+function multiply(val1, val2) {
+  return val1 * val2
 }
