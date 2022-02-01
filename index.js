34a60bb6f0058b82198c7a4c0b261cdad8c38922
Walmyr <wlsf82@gmail.com>
2022-02-01 17:30:25 +0100

    And another bit more

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 module.exports = function(items) {
-  if (!items.length) return 0
   const totalPrice = items.map(item => {
     const { precoUnitario, quantidadeVendida } = item
     return precoUnitario * quantidadeVendida
-  }).reduce((a, b) => a + b)
+  }).reduce((a, b) => a + b, 0)
 
   return totalPrice * .05
 
